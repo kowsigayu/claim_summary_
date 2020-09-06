df902ea41b08c0e8d5f0898fcd80e8a6f90ae898
K <802373@cognizant.com>
2020-09-06 19:42:37 +0530

    Added Manual Date Validation

--- javaScript/script.js
+++ javaScript/script.js
@@ -2,6 +2,8 @@ function loginValidation() {
     let userid = document.getElementById('user-id').value;
     let password = document.getElementById('user-pass').value;
     let passFlag, idFlag;
+
+    // Password Validation
     if(password.length <= 6) {
         document.getElementById("password").innerHTML="Password should be atleast 6 characters";
         passFlag = false;
@@ -9,6 +11,8 @@ function loginValidation() {
         document.getElementById("password").innerHTML = "";
         passFlag = true;
     }
+
+    // UserId Validation
     if(userid == null || userid == "") {
         document.getElementById("Empid").innerHTML="Employee Id cannot be Empty";
         idFlag = false;
@@ -26,18 +30,25 @@ function loginValidation() {
     if(idFlag && passFlag) {
         location.href="view_claim.html";
     }
+    // setTimeout(() => { maskClaimNumber(); }, 100);
 }
 function viewclaim() {
     location.href="update.html";
 }
+
+// function maskClaimNumber() {
+//     alert("id");
+//     document.getElementById('mask').innerHTML="XXX-XXX-XXXX";
+// }
 
 function UpdatePageValidation() {
     let claimNo = document.getElementById('claimNo').value;
     let claimProgram = document.getElementById('claimProgram').value;
     let startDate = document.getElementById('startDate').value;
     let endDate = document.getElementById('endDate').value;
-    let claimnumcheck, claimprgmCheck, datecheck;
+    let claimnumcheck, claimprgmCheck, datecheck = true;
     
+    // Claim Number Validation
     if(claimNo.length <= 9 && claimNo == "") {
         document.getElementById("claimNoError").innerHTML="Claim Number should be maximum length of 9";
         claimnumcheck = false;
@@ -48,6 +59,8 @@ function UpdatePageValidation() {
         document.getElementById("claimNoError").innerHTML = "";
         claimnumcheck = true;
     }
+
+    // Claim Program Validation
     if(claimProgram.length < 20 && claimProgram == "") {
         document.getElementById("claimProgramError").innerHTML="Claim Program should be maximum length of 20";
         claimprgmCheck = false;
@@ -55,28 +68,30 @@ function UpdatePageValidation() {
         document.getElementById("claimProgramError").innerHTML = "";
         claimprgmCheck = true;
     }
-    if (Date.parse(startDate) > Date.parse(endDate)) {
-        datecheck = false;
-        document.getElementById("endDateError").innerHTML = "End Date cannot be lesser than Start Date";
-    }
+
+    // Date Validation
     if(!startDate){
         datecheck = false;
         document.getElementById("startDateError").innerHTML = "StartDate cannot be empty";
     } else {
-        datecheck = true;
         document.getElementById("startDateError").innerHTML = "";
     }
     if(!endDate){
         datecheck = false;
         document.getElementById("endDateError").innerHTML = "EndDate cannot be empty";
     } else {
-        datecheck = true;
         document.getElementById("endDateError").innerHTML = "";
     }
+    if (datecheck && Date.parse(startDate) > Date.parse(endDate)) {
+        datecheck = false;
+        document.getElementById("endDateError").innerHTML = "End Date cannot be lesser than Start Date";
+    }
+
     if(datecheck && claimprgmCheck && claimnumcheck) {
         location.href="view_claim.html";
     }
 }
+
 function cancel() {
     location.href="update.html";
 }
